9b49557a25459c2ecd14a2c52655e3ba54bb368e
Lily <ms.lilichka@yandex.ru>
2022-08-05 16:29:06 +0500

    Написала метод по отбору из массива определенных элементов по длине символов

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -10,6 +10,22 @@
 [1234,1567,-2,computer science] -> ["-2"]
 [Russia,Denmark,Kazan] -> [] */
 
+
+void ArraySort(string[] arr)
+{
+    int size = 3;
+    var value = string.Empty;
+    string[] newarr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        value = arr[i];
+        if (value.Length <= size)
+            newarr[i] = arr[i];
+    }
+    newarr = newarr.Where(x => x != null).ToArray();
+    Console.WriteLine($"Отобранный массив: [{string.Join(", ", newarr)}]");
+}
+
 string[] CreateArray()
 {
     Console.WriteLine("Введите элементы массива через запятую: ");
