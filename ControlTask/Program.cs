94cb2d939cf733c1f743b7acd3e045297ccc1229
Lily <ms.lilichka@yandex.ru>
2022-08-05 16:25:39 +0500

    Нарисовала блок схемы к задаче

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -6,7 +6,8 @@
 лучше обойтись исключительно массивами.
 
 **Примеры**:
-["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-["1234", "1567", "-2", "computer science"] -> ["-2"]
-["Russia", "Denmark", "Kazan"] -> [] */
+[hello,2,world,:-)] -> ["2", ":-)"]
+[1234,1567,-2,computer science] -> ["-2"]
+[Russia,Denmark,Kazan] -> [] */
+
 
